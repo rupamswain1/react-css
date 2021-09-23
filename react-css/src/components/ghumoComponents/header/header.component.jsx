309eefe931fb95e1aa3420c4d8009c6288fc0c62
rupamswain1 <rupamswain1@gmail.com>
2021-09-24 01:19:33 +0530

    header background created

---
+++ react-css/src/components/ghumoComponents/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.style.css'
+function Header() {
+    return (
+        <header className='headerMain'>
+            Header
+        </header>
+    )
+}
+
+export default Header
---
+++ react-css/src/pages/ghumo/ghumo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/ghumoComponents/header/header.component';
+import './ghumo.css';
+
+function GhumoPage() {
+    return (
+        <div className='bodyContainer'>
+            <Header/>
+        </div>
+    )
+}
+
+export default GhumoPage;
