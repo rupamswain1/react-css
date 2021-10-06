5da7422c0bd9c161102f098b741748ba46b93100
rupamswain1 <rupamswain1@gmail.com>
2021-10-07 00:47:03 +0530

    about pafe is creates

--- react-css/src/components/ghumoComponents/cards/cards.jsx
+++ react-css/src/components/ghumoComponents/cards/cards.jsx
@@ -3,7 +3,12 @@ import './cards.style.scss';
 function Cards() {
     return (
         <div className='card'>
-            This is a card
+            <div className="card__side card__side--front card__side--front--1">
+            Front...
+            </div>
+            <div className="card__side card__side--back card__side--back--1">
+            Back
+            </div>
         </div>
     )
 }
