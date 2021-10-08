0551f83f175e4e675512fff5884ef413fcfc80f0
rupamswain1 <rupamswain1@gmail.com>
2021-10-09 01:11:03 +0530

    tour section completed

--- react-css/src/components/ghumoComponents/cards/cards.jsx
+++ react-css/src/components/ghumoComponents/cards/cards.jsx
@@ -2,36 +2,37 @@ import React from 'react';
 import Button from '../button/button';
 import './cards.style.scss';
 import img1 from '../../../assets/ghumoAssets/img/nat-4.jpg';
-function Cards({heading,details,price,image}) {
+function Cards({sno,heading,details,price,image}) {
+    
     return (
         <div className='card'>
-            <div className="card__side card__side--front card__side--front--1">
-                <div className='card__picture card__picture--1'>
+            <div className={`card__side card__side--front card__side--front--${sno}`}>
+                <div className={`card__picture card__picture--${sno}`}>
                     &nbsp;
                 </div>
                 <h4 className="card__heading">
-                    <span className='card__heading--span card__heading--span--1'>
-                        the sea explorer
+                    <span className={`card__heading--span card__heading--span--${sno}`}>
+                        {heading}
                     </span>
                 </h4>
                 <span className='card__details'>
                     <ul>
-                        <li>3 days tours</li>
-                        <li>Up to 30 People</li>
-                        <li>2 tour guides</li>
-                        <li>Sleep in cozy hotel</li>
-                        <li>Difficulty: Easy</li>
+                        {details.map(d=>{
+                            return(
+                                <li>{d.toUpperCase()}</li>
+                            )
+                        })}
                     </ul>
                 </span>
             </div>
-            <div className="card__side card__side--back card__side--back--1">
+            <div className={`card__side card__side--back card__side--back--${sno}`}>
                 <div className="card__cta">
                     <div className='card__price-box'>
                         <p className='card__price-only'>
                             Only
                         </p>
                         <p className='card__price-value'>
-                            $297
+                            {price}
                         </p>
                     </div>
                     <Button text='book now' style='card__btn'/>
--- react-css/src/components/ghumoComponents/toursSection/ toursSection.jsx
+++ react-css/src/components/ghumoComponents/toursSection/ toursSection.jsx
@@ -3,16 +3,59 @@ import SecondaryHeader from '../secondaryHeader/secondaryHeader';
 import Cards from '../cards/cards';
 import './toursSection.style.scss'
 
+const cardData=[
+    {
+        sno:1,
+        heading:'the sea explorer ',
+        details:[
+            '3 days tours',
+            'Up to 30 People',
+            '2 tour guides',
+            'sleep in cozy hotel',
+            'Difficulty: Easy'
+        ],
+        price:'$297'
+    },
+    {
+        sno:2,
+        heading:'the forest hiker',
+        details:[
+            '7 days tours',
+            'Up to 40 People',
+            '6 tour guides',
+            'sleep in provided tents',
+            'Difficulty: Moderate'
+        ],
+        price:'$297'
+    },
+    {
+        sno:3,
+        heading:'the Snow Adventure',
+        details:[
+            '5 days tours',
+            'Up to 15 People',
+            '3 tour guides',
+            'sleep in Provided tents',
+            'Difficulty: Hard'
+        ],
+        price:'$297'
+    }
+]
+
 function  ToursSection() {
     return (
         <section className="section-tours">
                 <SecondaryHeader textContent='most popular tours' Style='u-test-center u-margin-bottom-big'/>
                 <div className="row">
-                    <div className="col-1-of-3"> 
-                        <Cards/>
-                    </div>
-                    <div className="col-1-of-3"> 1 OF 3</div>
-                    <div className="col-1-of-3"> 1 OF 3</div>
+                    
+                        {cardData.map((c)=>{
+                            return(
+                                <div className="col-1-of-3"> 
+                                    <Cards {...c}/>
+                                </div>
+                            );
+                        })
+                        }
                 </div>
         </section>
     )
