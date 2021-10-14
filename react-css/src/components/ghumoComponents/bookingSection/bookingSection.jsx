978bb6e94bb02fb77cb94152cf8a4c036b233e50
rupamswain1 <rupamswain1@gmail.com>
2021-10-14 11:33:32 +0530

    Booking Section added

--- react-css/src/components/ghumoComponents/bookingSection/bookingSection.jsx
+++ react-css/src/components/ghumoComponents/bookingSection/bookingSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SecondaryHeader from '../secondaryHeader/secondaryHeader';
+import Button from '../button/button';
 import './bookingSection.style.scss';
 
 function BookingSection() {
@@ -18,6 +19,27 @@ function BookingSection() {
                             <div className='form__group'>
                                 <input type='email' className='form__input' placeholder='Email' id='email' name='email  ' required/>
                                 <label for='email' className='form__label'>Email Address</label>
+                            </div>
+                            <div className='form__group'>
+                                <div className='form__radio-group'>
+                                    <input type='radio' className='form__radio-input' id='small' name='book-radio'/>
+                                    <label for="small" className='form__radio-lable'>
+                                        <span class='form__radio-button'></span> 
+                                        Small tour group
+                                    </label>
+                                    
+                                 </div>
+                                 <div className='form__radio-group'>
+                                    <input type='radio' className='form__radio-input' id='large'  name='book-radio'/>
+                                    <label for="large" className='form__radio-lable'>
+                                        <span className='form__radio-button'></span>
+                                        Large tour group
+                                    </label> 
+                                    
+                                 </div>
+                                 <div className='form__group u-test-center'>
+                                    <Button text='Next Steps &rarr;' style='btn-green u-margin-top-medium u-test-center'/>
+                                 </div>
                             </div>
                         </form>
                     </div>
