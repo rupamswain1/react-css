363e284bd6544774e3799918f877020661273832
rupamswain1 <rupamswain1@gmail.com>
2021-10-17 01:00:37 +0530

    navigation done

--- react-css/src/components/ghumoComponents/navigation/navigation.jsx
+++ react-css/src/components/ghumoComponents/navigation/navigation.jsx
@@ -4,7 +4,9 @@ function Navigation() {
     return (
         <div className='navigation'>
             <input type='checkbox' id='navi-toggle' className='navigation__checkbox'/>
-            <label for='navi-toggle' className='navigation__button'>Menu</label>
+            <label for='navi-toggle' className='navigation__button'>
+                <span className='navigation__icon'>&nbsp;</span>
+            </label>
             <div className='navigation__background'>&nbsp;</div>
             <nav className='navigation__nav'>
                 <ul className='navigation__list'>
