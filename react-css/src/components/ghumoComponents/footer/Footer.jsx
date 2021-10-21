88dc438734c0fd7e2b1f8ef818844c99e31f76a7
rupamswain1 <rupamswain1@gmail.com>
2021-10-21 23:23:59 +0530

    resposnsive image added for footer

--- react-css/src/components/ghumoComponents/footer/Footer.jsx
+++ react-css/src/components/ghumoComponents/footer/Footer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './footer.style.scss';
 import Paragraph from '../paragraph/paragraph'
-import logo from '../../../assets/ghumoAssets/img/logo-green-2x.png';
+import logo from '../../../assets/ghumoAssets/img/logo-green-small-2x.png';
+import logoOneX from '../../../assets/ghumoAssets/img/logo-green-small-1x.png';
+import imgLogo from '../../../assets/ghumoAssets/img/logo-green-2x.png';
+import imgLogoOneX from '../../../assets/ghumoAssets/img/logo-green-1x.png';
 function Footer() {
     return (
         <footer className='footer'>
             <div className='footer__logo-box'>
-                <img src={logo} alt='Logo' className='footer__logo'/>
+                <picture className='footer__logo'>
+                    <source srcSet={`${logoOneX} 1x,${logo} 2x`} media="(max-width:37.5em)"></source>
+                    <img srcSet={`${imgLogoOneX} 1x,${imgLogo} 2x`} alt='Logo' className='footer__logo'/>
+                </picture>
+                
             </div>
             <div className='row'>
                 <div className='col-1-of-2'>
