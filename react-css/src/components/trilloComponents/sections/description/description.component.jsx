522557bfcf81235b57e10144c30194d33248aa43
rupamswain1 <rupamswain1@gmail.com>
2021-11-16 01:09:40 +0530

    package update

--- react-css/src/components/trilloComponents/sections/description/description.component.jsx
+++ react-css/src/components/trilloComponents/sections/description/description.component.jsx
@@ -3,7 +3,7 @@ import Paragraph from '../../components/paragraph/paragraph.component';
 import './description.style.scss'
 
 let paragrah1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"
-let paragraph2: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+let paragraph2= "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
 function Description() {
     return (
         <div className='description'>
