8ee6f720141ebfa59e285adb317992ae1f677ff0
rupamswain1 <rupamswain1@gmail.com>
2021-11-20 23:29:29 +0530

    nexter image added

--- react-css/src/App.js
+++ react-css/src/App.js
@@ -1,5 +1,5 @@
 
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import {BrowserRouter,Route,Routes,HashRouter} from 'react-router-dom';
 import './App.css';
 import RoutesPage from './pages/routePages/routesPage';
 import GhumoPage from './pages/ghumo/ghumo';
@@ -7,13 +7,15 @@ import Trillo from './pages/trilloPages/trillo';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        {/* Hashrouter is used in place of browser router as github pades does not support routing, when we use the hashRouter # added to url like 
+        http://abc/#/path/ */}
+        <HashRouter> 
             <Routes>
               <Route path='/react-css' element={<RoutesPage/>}/>
               <Route path='/ghumo' element={<GhumoPage/>}/>
               <Route path='trillo' element={<Trillo/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
